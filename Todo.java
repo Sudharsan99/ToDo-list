cd2f0cb9be7a2c8fc2e73b07fcdb86a0b7a2eac5
Sudharsan99 <51789654+Sudharsan99@users.noreply.github.com>
2024-04-01 13:50:20 +0530

    Add files via upload

---
+++ Todo.java
@@ -0,0 +1,27 @@
+package com.project.todoList.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    private boolean completed;
+}
